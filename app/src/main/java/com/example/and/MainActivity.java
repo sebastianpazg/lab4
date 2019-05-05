01b4539e232df4e20421c4b02538a71fc5860744
bartek8654 <uttsmile8654@gmail.com>
2019-05-05 19:38:33 +0200

    Second Edit

--- app/src/main/java/com/example/and/MainActivity.java
+++ app/src/main/java/com/example/and/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.and;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -32,6 +36,23 @@ public class MainActivity extends AppCompatActivity {
                 R.id.View );
         listview.setAdapter(this.adapter);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public void nowyWpis(MenuItem mi)
+    {
+        Intent intencja = new Intent(this,
+                DodajWpis.class);
+        startActivityForResult(intencja, 1);
+    }
+
+
 }
 
 //TODO
